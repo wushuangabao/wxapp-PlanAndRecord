d49f1de511b5c2ebce9ef605d8f548bc0fed0172
无双BaOY_WHA <445395697@qq.com>
2019-05-30 20:48:11 +0800

    update: 优先使用缓存中的planData

--- miniprogram/pages/plan/index/index.js
+++ miniprogram/pages/plan/index/index.js
@@ -1,6 +1,6 @@
 // miniprogram/pages/plan/index/index.js
 
-const tabNames = ["事务管理", "日程管理"],
+const tabNames = ["事务管理", "日程管理", "项目管理"],
   appData = getApp().globalData,
   AV = getApp().AV,
   convertEvtTab = data => ({
@@ -17,7 +17,6 @@ const tabNames = ["事务管理", "日程管理"],
   });
 
 Page({
-
   data: {
     // 屏幕高度
     height: 1400,
@@ -97,9 +96,13 @@ Page({
       });
     } else {
       if (this.data.currentItem != -1) {
+        // 修改事务
         this.deleteEvent();
+        this.addEvent(item, this.data.evtTabName);
+      } else {
+        // 新增事务
+        this.addEvent(item);
       }
-      this.addEvent(item, this.data.evtTabName);
       this.setData({
         showPopup: false
       });
@@ -188,6 +191,247 @@ Page({
     }
   },
 
+  ///////////////////////
+  /// 新增事务
+  ///////////////////////
+  addEvent(item, evtTabName) {
+    var lists = this.data.lists,
+      id = -1;
+    if (arguments.length > 1) {
+      let evtTabNames = this.data.evtTabNames;
+      // 查找item对应的evtTabNames中的下标
+      for (var i = 0; i < evtTabNames.length; i++) {
+        if (evtTabNames[i] == evtTabName)
+          id = i;
+      }
+      if (id === -1) return;
+    } else {
+      id = this.data.evtTabId;
+    }
+    if (this.data.subObjId == id) {
+      // 赋值list
+      let list = this.data.list;
+      list.push(item);
+      this.setData({
+        list: list
+      });
+    } else {
+      // 赋值lists
+      lists[id].push(item);
+      this.setData({
+        lists: lists
+      });
+    }
+    this.setHeight();
+  },
+
+  //////////////////////
+  /// 删除事务
+  //////////////////////
+  deleteEvent() {
+    let list = this.data.list;
+    list.splice(this.data.currentItem, 1);
+    this.setData({
+      list: list
+    });
+    this.setHeight();
+  },
+
+  ///////////////////////
+  /// 页面初始化1
+  ///////////////////////
+  onLoad(options) {
+    let planData = wx.getStorageSync('planData');
+    if (planData) {
+      console.log("直接使用缓存中的planData...");
+      this.setData(planData);
+      this.changeTabs({
+        detail: {
+          activeKey: 0,
+          activeSubKey: 0
+        }
+      });
+    } else {
+      // 获取屏幕高度(rpx)
+      let that = this;
+      wx.getSystemInfo({
+        success: function(res) {
+          let clientHeight = res.windowHeight;
+          let clientWidth = res.windowWidth;
+          let ratio = 750 / clientWidth;
+          let height = clientHeight * ratio;
+          that.setData({
+            vHeight: height - 300
+          });
+        }
+      });
+      this.setHeight();
+    }
+    this.openid = wx.getStorageSync('openid');
+  },
+
+  ///////////////////////
+  /// 页面初始化2
+  ///////////////////////
+  onReady() {
+    this.componentTabs = this.selectComponent('#tabs');
+    // this.crtEventData();
+    // this.crtTimeTabs();
+    // 上面两个方法是使用云开发。
+    // 现替换为下面的，使用leanCloud数据库：
+    if (this.data.dubbleTabs.length == 0) {
+      new AV.Query('EvtTabs')
+        .find()
+        .then(tabs => {
+          var data = tabs.map(convertEvtTab);
+          // 创建事务标签
+          this.crtEvtTabs(data);
+          var query = new AV.Query('EvtItems');
+          query.equalTo('openid', this.openid);
+          query.find()
+            .then(items => {
+              var data = items.map(convertEvtItem);
+              // 创建事务内容（todolist）
+              this.crtEvtLists(data);
+              // 创建日程标签 TODO
+              this.crtTimeTabs();
+              // 刷新标签（重新渲染）
+              this.resetTabs();
+              console.log("已完成plan数据的下载！");
+            })
+            .catch(console.error);
+        })
+        .catch(console.error);
+    }
+  },
+
+  /////////////////////////
+  /// 创建 事务管理 内容
+  /////////////////////////
+  crtEvtLists(data) {
+    var sizeEvtTabs = this.data.sizeEvtTabs,
+      lists = new Array(sizeEvtTabs),
+      len = data.length;
+    for (var i = 0; i < sizeEvtTabs; i++) {
+      lists[i] = [];
+    }
+    if (len > 0) {
+      for (var i = 0; i < len; i++) {
+        var tabId = data[i].tab;
+        if (tabId >= 0 && tabId < this.data.sizeEvtTabs) {
+          let item = {
+            name: data[i].name
+          };
+          if (data[i].description) {
+            item['description'] = data[i].description;
+          }
+          if (data[i].tag) {
+            item['tag'] = data[i].tag;
+          }
+          lists[tabId].push(item);
+        }
+      }
+      this.setData({
+        lists: lists
+      });
+    }
+  },
+
+  /////////////////////////
+  /// 创建 事务管理 标签
+  /////////////////////////
+  crtEvtTabs(data) {
+    var evtTabNames = [],
+      descriptions = [],
+      dubbleTabs = [],
+      len = data.length;
+    if (len > 0) {
+      for (var i = 0; i < len; i++) {
+        let dubbleTab = {
+          tab: tabNames[0],
+          key: "事务管理",
+          subKey: data[i].id.toString(), //如果不转化为字符串，subKey==0这项会无法显示
+          subTab: data[i].name
+        };
+        dubbleTabs.push(dubbleTab);
+        descriptions.push(data[i].name + "：" + data[i].description);
+        evtTabNames.push(data[i].name);
+      }
+      this.setData({
+        dubbleTabs: dubbleTabs,
+        evtDescriptions: descriptions,
+        evtTabNames: evtTabNames,
+        sizeEvtTabs: len
+      });
+    }
+  },
+
+  ////////////////////////
+  /// 刷新标签（重新渲染）
+  ////////////////////////
+  resetTabs() {
+    // 初始化当前被选中的标签
+    this.changeTabs({
+      detail: {
+        activeKey: 0,
+        activeSubKey: 0
+      }
+    });
+    // 调用自定义组件tabs的方法
+    this.componentTabs.initTabs();
+  },
+
+  //////////////////////////////
+  /// 创建 日程管理 标签
+  //////////////////////////////
+  crtTimeTabs() {
+    var dubbleTabs = this.data.dubbleTabs;
+    dubbleTabs.push({
+      tab: tabNames[1],
+      key: "日程管理：从时间的维度，锁定重要的事情。"
+    });
+    this.setData({
+      dubbleTabs: dubbleTabs
+    });
+  },
+
+  /////////////////////
+  /// 设置tabs的高度
+  /////////////////////
+  setHeight() {
+    let maxSize = 0,
+      height = 352,
+      lists = this.data.lists;
+    for (var i = 0; i < lists.length; i++) {
+      if (lists[i].length > maxSize)
+        maxSize = lists[i].length;
+    }
+    if (maxSize > 4) {
+      height = 88 * maxSize;
+      // 超出屏幕，提示事务太多
+      if (height > this.data.vHeight) {
+        //不做限制height = this.data.vHeight;
+        wx.lin.showMessage({
+          duration: 2500,
+          type: 'warning',
+          content: '事情太多了，考虑扔掉一些吧！'
+        });
+      }
+    }
+    this.setData({
+      height: height
+    });
+  },
+
+
+  ////////////////////
+  /// 离开页面
+  ////////////////////
+  onHide() {
+    console.log("onHide");
+    wx.setStorageSync('planData', this.data);
+  },
+
   //////////////////////////////
   /// 根据appData.toDoLists
   /// 给页面的 事务管理数据 赋值
@@ -251,217 +495,5 @@ Page({
         sizeEvt: len
       });
     }
-  },
-
-  //////////////////////////////
-  /// 给页面的 日程管理数据 赋值
-  //////////////////////////////
-  crtTimeTabs() {
-    var dubbleTabs = this.data.dubbleTabs;
-    dubbleTabs.push({
-      tab: tabNames[1],
-      key: "日程管理：从时间的维度，锁定重要的事情。"
-    });
-    this.setData({
-      dubbleTabs: dubbleTabs
-    });
-  },
-
-  ///////////////////////
-  /// 新增事务
-  ///////////////////////
-  addEvent(item, evtTabName) {
-    var lists = this.data.lists,
-      evtTabNames = this.data.evtTabNames,
-      id = -1;
-    // 查找item的tag对应的evtTabNames中的下标
-    for (var i = 0; i < evtTabNames.length; i++) {
-      if (evtTabNames[i] == evtTabName)
-        id = i;
-    }
-    if (id === -1) return;
-
-    if (this.data.subObjId == id) {
-      // 赋值list
-      let list = this.data.list;
-      list.push(item);
-      this.setData({
-        list: list
-      });
-    } else {
-      // 赋值lists
-      lists[id].push(item);
-      this.setData({
-        lists: lists
-      });
-    }
-
-    this.setHeight();
-  },
-
-  //////////////////////
-  /// 删除事务
-  //////////////////////
-  deleteEvent() {
-    let list = this.data.list;
-    list.splice(this.data.currentItem, 1);
-    this.setData({
-      list: list
-    });
-    this.setHeight();
-  },
-
-  ///////////////////////
-  /// 页面初始化1
-  ///////////////////////
-  onLoad(options) {
-    // 获取屏幕高度(rpx)
-    let that = this;
-    wx.getSystemInfo({
-      success: function(res) {
-        let clientHeight = res.windowHeight;
-        let clientWidth = res.windowWidth;
-        let ratio = 750 / clientWidth;
-        let height = clientHeight * ratio;
-        that.setData({
-          vHeight: height - 300
-        });
-      }
-    });
-    this.setHeight();
-    this.openid = wx.getStorageSync('openid');
-  },
-
-  ///////////////////////
-  /// 页面初始化2
-  ///////////////////////
-  onReady() {
-    this.componentTabs = this.selectComponent('#tabs');
-    // this.crtEventData();
-    // this.crtTimeTabs();
-    // 上面两个方法是使用云开发。
-    // 现替换为下面的，使用leanCloud数据库：
-    new AV.Query('EvtTabs')
-      .find()
-      .then(tabs => {
-        var data = tabs.map(convertEvtTab);
-        this.crtEvtTabs(data);
-        var query = new AV.Query('EvtItems');
-        query.equalTo('openid', this.openid);
-        query.find()
-          .then(items => {
-            var data = items.map(convertEvtItem);
-            this.crtEvtLists(data);
-            this.resetTabs();
-          })
-          .catch(console.error);
-      })
-      .catch(console.error);
-  },
-
-  ///////////////////////
-  /// 创建toDoLists
-  ///////////////////////
-  crtEvtLists(data) {
-    var sizeEvtTabs = this.data.sizeEvtTabs,
-      lists = new Array(sizeEvtTabs),
-      len = data.length;
-    for (var i = 0; i < sizeEvtTabs; i++) {
-      lists[i] = [];
-    }
-    if (len > 0) {
-      for (var i = 0; i < len; i++) {
-        var tabId = data[i].tab;
-        if (tabId >= 0 && tabId < this.data.sizeEvtTabs) {
-          let item = {
-            name: data[i].name
-          };
-          if (data[i].description) {
-            item['description'] = data[i].description;
-          }
-          if (data[i].tag) {
-            item['tag'] = data[i].tag;
-          }
-          lists[tabId].push(item);
-        }
-      }
-      this.setData({
-        lists: lists
-      });
-    }
-  },
-
-  //////////////////////
-  /// 创建事务标签
-  //////////////////////
-  crtEvtTabs(data) {
-    var evtTabNames = [],
-      descriptions = [],
-      dubbleTabs = [],
-      len = data.length;
-    if (len > 0) {
-      for (var i = 0; i < len; i++) {
-        let dubbleTab = {
-          tab: tabNames[0],
-          key: "事务管理",
-          subKey: data[i].id.toString(), //如果不转化为字符串，subKey==0这项会无法显示
-          subTab: data[i].name
-        };
-        dubbleTabs.push(dubbleTab);
-        descriptions.push(data[i].name + "：" + data[i].description);
-        evtTabNames.push(data[i].name);
-      }
-      this.setData({
-        dubbleTabs: dubbleTabs,
-        evtDescriptions: descriptions,
-        evtTabNames: evtTabNames,
-        sizeEvtTabs: len
-      });
-    }
-  },
-
-  //////////////////////
-  /// 刷新标签
-  //////////////////////
-  resetTabs() {
-    this.crtTimeTabs(); //临时凑数 TODO
-    // 初始化当前标签
-    this.changeTabs({
-      detail: {
-        activeKey: 0,
-        activeSubKey: 0
-      }
-    });
-    // 调用自定义组件tabs的方法
-    this.componentTabs.initTabs();
-  },
-
-  /////////////////////
-  /// 设置tabs的高度
-  /////////////////////
-  setHeight() {
-    let maxSize = 0,
-      height = 352,
-      lists = this.data.lists;
-    for (var i = 0; i < lists.length; i++) {
-      if (lists[i].length > maxSize)
-        maxSize = lists[i].length;
-    }
-    if (maxSize > 4) {
-      height = 88 * maxSize;
-      // 超出屏幕的部分不可见，防止事务太多
-      if (height > this.data.vHeight) {
-        height = this.data.vHeight;
-        wx.lin.showMessage({
-          duration: 2500,
-          type: 'warning',
-          content: '事情太多了，考虑扔掉一些吧！'
-        });
-      }
-    }
-    this.setData({
-      height: height
-    });
-  },
-
+  }
 })
